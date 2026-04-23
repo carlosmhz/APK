13e75f9914db6fde5af72080b5e44840a216d9fb
carlosmhz <carlos.slsh@gmail.com>
2026-04-23 15:45:27 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "cracovia-v1";
+
+const urlsToCache = [
+  "./",
+  "./planning-jose-miguel.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
